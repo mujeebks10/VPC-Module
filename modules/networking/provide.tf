4d6127a6c309196d3166ef46a5fa1af9a1179c08
mujeebks10 <mujeebks10@gmail.com>
2025-09-25 10:29:41 +0100

    Modified backend config

---
+++ modules/networking/provide.tf
@@ -0,0 +1,24 @@
+######## ####### Configure the Terraform Backend ###########
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+    backend "s3" {
+    bucket         = "my-terraform-statebucket01"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+
+}
+
+#### Configure the AWS Provider
+provider "aws" {
+  region  = "us-east-1" # Specify the AWS region
+  profile = "admin"     # Replace with the name of your AWS CLI profile
+}
