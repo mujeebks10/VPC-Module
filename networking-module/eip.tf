e039d89dec4f934dcf2735f5da537076cbc9b5b4
mujeebks10 <mujeebks10@gmail.com>
2026-01-18 17:44:06 +0000

    EIP dynamic updated

--- networking-module/eip.tf
+++ networking-module/eip.tf
@@ -16,12 +16,12 @@ resource "aws_eip" "web_tier2_eip2" {
   }
 }
 
-############ OR EIP ##########
-resource "aws_eip" "web_tier" {
-  for_each = toset(var.web_subnets)
-  domain   = "vpc"
-  tags = {
-    Name = each.key == var.web_subnets[0] ? var.web_tier1_eip1 : var.web_tier2_eip2
-  }
-}
+# ############ OR EIP ##########
+# resource "aws_eip" "web_tier" {
+#   for_each = toset(var.web_subnets)
+#   domain   = "vpc"
+#   tags = {
+#     Name = each.key == var.web_subnets[0] ? var.web_tier1_eip1 : var.web_tier2_eip2
+#   }
+# }
 
